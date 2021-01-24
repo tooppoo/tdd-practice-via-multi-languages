588ced64572874bef35d47c37371ddab8cfcc47c
philomagi <tooppoo56@gmail.com>
2021-01-25 01:31:08 +0900

    delete unnecessary codes

---
+++ golang/src/chapter-11/money.go
@@ -0,0 +1,30 @@
+package money
+
+type Money struct {
+	amount   int
+	currency string
+}
+
+func NewDollar(amount int) Money {
+	return newMoney(amount, "USD")
+}
+
+func NewFranc(amount int) Money {
+	return newMoney(amount, "CHF")
+}
+func newMoney(amount int, currency string) Money {
+	return Money{
+		amount:   amount,
+		currency: currency,
+	}
+}
+
+func (m Money) Times(num int) Money {
+	return Money{
+		amount:   m.amount * num,
+		currency: m.currency,
+	}
+}
+func (m Money) Currency() string {
+	return m.currency
+}
---
+++ golang/src/chapter-11/money_test.go
@@ -0,0 +1,23 @@
+package money
+
+import (
+	"test/assert"
+	"testing"
+)
+
+func TestMultiplication(t *testing.T) {
+	five := NewDollar(5)
+
+	assert.Equals(NewDollar(10), five.Times(2), t)
+	assert.Equals(NewDollar(15), five.Times(3), t)
+}
+
+func TestEquality(t *testing.T) {
+	assert.True(NewDollar(5) == NewDollar(5), t)
+	assert.False(NewDollar(5) == NewDollar(6), t)
+}
+
+func TestCurrency(t *testing.T) {
+	assert.Equals("USD", NewDollar(5).Currency(), t)
+	assert.Equals("CHF", NewFranc(5).Currency(), t)
+}
